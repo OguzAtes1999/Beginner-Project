84dbb4ec6692b27f42a7f713c664fc9ff52b6756
OguzAtes1999 <oguz.ates@rwth-aachen.de>
2022-09-26 16:54:24 +0200

    First Project, word reversal. Day 1

---
+++ WordReversal/WordReversal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+public class WordReversal
+{
+ 
+       public static void main (String[] args)
+       {
+             Scanner sc= new Scanner(System.in); //System.in is a standard input stream
+             System.out.print("Enter a string:");
+             String str=sc.nextLine(); //reads String
+             System.out.println("The word to be reversed is: "+ str);
+             String reversed=new String("");
+
+            for(int i=str.length()-1, j= 0; i>=0 && j<=str.length() ;i--,j++){
+               
+
+               reversed=reversed+String.valueOf(str.charAt(i));
+                
+            }
+             System.out.println("The reversed version is: "+reversed);
+                
+       }
+}
